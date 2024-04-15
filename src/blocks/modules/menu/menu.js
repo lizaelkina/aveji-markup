eaf6fa5161bf393fff5f3a94a40f5b690d500916
Liza Elkina <lizapetkova@yandex.ru>
2024-04-15 21:32:17 +0300

    feature: close menu by esc and by overlay

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -1,16 +1,28 @@
 const page = document.querySelector(".page");
 const menu = page.querySelector(".menu");
+const clickForOverlay = page.querySelector(".overlay");
 const buttonOpenMenu = page.querySelector(".header__burger");
 const buttonCloseMenu = page.querySelector(".btn_menu_close");
 
+function closeMenuByEsc(event) {
+    if (event.key === "Escape") {
+        menu.querySelector(".menu_opened");
+        closeMenu();
+    }
+}
+
 function openMenu() {
     menu.classList.add("menu_opened");
+    document.addEventListener("keydown", closeMenuByEsc);
 }
 
 function closeMenu() {
     menu.classList.remove("menu_opened");
+    document.removeEventListener("keydown", closeMenuByEsc);
 }
 
 buttonOpenMenu.addEventListener("click", openMenu);
 
 buttonCloseMenu.addEventListener("click", closeMenu);
+
+clickForOverlay.addEventListener("click", closeMenu);
