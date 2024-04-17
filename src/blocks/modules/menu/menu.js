389d489d1b1ab0b1de6defc0b59528a4748ecc54
Liza Elkina <lizapetkova@yandex.ru>
2024-04-17 14:00:38 +0300

    chore: refactor

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -4,13 +4,6 @@ const clickForOverlay = page.querySelector(".overlay");
 const buttonOpenMenu = page.querySelector(".header__burger");
 const buttonCloseMenu = page.querySelector(".btn_menu_close");
 
-function closeMenuByEsc(event) {
-    if (event.key === "Escape") {
-        menu.querySelector(".menu_opened");
-        closeMenu();
-    }
-}
-
 function openMenu() {
     menu.classList.add("menu_opened");
     document.addEventListener("keydown", closeMenuByEsc);
@@ -19,6 +12,13 @@ function openMenu() {
 function closeMenu() {
     menu.classList.remove("menu_opened");
     document.removeEventListener("keydown", closeMenuByEsc);
+}
+
+function closeMenuByEsc(event) {
+    if (event.key === "Escape") {
+        menu.querySelector(".menu_opened");
+        closeMenu();
+    }
 }
 
 buttonOpenMenu.addEventListener("click", openMenu);
