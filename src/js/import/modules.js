ae7dac46c4fe15450ad261ee60ae32c9c7a0f4cd
Liza Elkina <lizapetkova@yandex.ru>
2024-04-17 23:22:26 +0300

    chore: delete linebreak-style

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,3 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer";
-import "%modules%/menu/menu";
+import '%modules%/header/header';
+import '%modules%/footer/footer';
+import '%modules%/menu/menu';
